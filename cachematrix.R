2099a34b2d30bfd35bf8e06bad2dba80317ea57e
Vasatika <vasatika@gmail.com>
2021-04-06 10:24:22 +0530

    My code for CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The makeCacheMatrix creates a special matrix that can cache it's inverse using the set, 
+# get, setinv and getinv to set the value of the matrix, get the value of the matrix, set 
+# the value of the inverse and get the value of the inverse of the matrix. 
 
-## Write a short comment describing this function
+#The cacheSolve function uses solve() to retrieve the inverse of the cache matrix. 
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+    get <- function() x
+    setinv <- function(inverse) m <<- inverse
+    getinv <- function() m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       m <- x$getinv()
+       if(!is.null(m)) {
+       message("getting cached data")
+       return(m)
+  }
+       data <- x$get()
+       m <- solve(data, ...) ## Return a matrix that is the inverse of 'x' using solve()
+       x$setinv(m)
+       m
 }
+
+#Assigned matrix with numbers 6, 7, 8, 9 to MatA with 2 rows and 1 columns. 
+#Assigned the inverse cache matrix value to InvMatA.
+#Solved to find inverse of cached matrix using cacheSolve(InvMatA).
+#Displayed chached matrix data.
+
+MatA <- matrix(c(6:9), 2, 2) #sqaure matrix of value 6, 7, 8, 9.
+InvMatA <- makeCacheMatrix(MatA)
+cacheSolve(InvMatA) #Inverse after computation 
+
+cacheSolve(InvMatA) #Inverse returned from cached data
